35494b2ae1e1602f9491be96a518c2f58a7f93e5
Patisonek <patrycja.warmuz@gmail.com>
2017-11-29 14:43:13 +0100

    Add Horizontal Sliders actions

--- ColorCreator/ViewController.swift
+++ ColorCreator/ViewController.swift
@@ -10,22 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var selectedColor: UIColor!
     
     @IBOutlet var previewContainer: UIView!
     
     @IBAction func actionRandomButton(_ sender: Any) {
         
-        previewContainer.backgroundColor = UIColor.randomColor()
+        var r : CGFloat = 0
+        var g : CGFloat = 0
+        var b : CGFloat = 0
+        var a : CGFloat = 0
+        
+        
+        UIColor.randomColor().getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        redSlider.value = Float(r)
+        greenSlider.value = Float(g)
+        blueSlider.value = Float(b)
+        
+        updateInterface()
+        
         
     }
     
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     
+    
+    @IBAction func actionSliderChanged(_ sender: UISlider) {
+    
+        updateInterface()
+    }
+    
+    
+    
+    func updateInterface(){
+        
+        
+        let selectedRed = CGFloat(redSlider.value)
+        let selectedGreen = CGFloat(greenSlider.value)
+        let selectedBlue = CGFloat(blueSlider.value)
+        
+        
+        selectedColor = UIColor(red: selectedRed, green: selectedGreen, blue: selectedBlue, alpha: 1.0)
+        
+        previewContainer.backgroundColor = selectedColor
+        
+    }
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateInterface()
     }
 
     override func didReceiveMemoryWarning() {
