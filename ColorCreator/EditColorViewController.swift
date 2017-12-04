1c9a6e92625b978000babb4a1b8241c94efe5bc1
Patisonek <patrycja.warmuz@gmail.com>
2017-12-04 13:15:39 +0100

    Add New Views Controller

--- ColorCreator/EditColorViewController.swift
+++ ColorCreator/EditColorViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class EditColorViewController: UIViewController {
     
     var selectedColor: UIColor!
     
---
+++ ColorCreator/TabBarViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TabBarViewController.swift
+//  ColorCreator
+//
+//  Created by Admin on 04/12/2017.
+//  Copyright © 2017 patisonek. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBar.tintColor = UIColor.red 
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
