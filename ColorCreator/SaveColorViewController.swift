933cf9448b48051f0b2e26bd09d4fb363614da0e
Patisonek <patrycja.warmuz@gmail.com>
2018-01-29 12:37:22 +0100

    Zapisz Kolor - text field - obsluga klwiatury ekranowej

--- ColorCreator/SaveColorViewController.swift
+++ ColorCreator/SaveColorViewController.swift
@@ -8,10 +8,24 @@
 
 import UIKit
 
-class SaveColorViewController: UITabBarController {
+class SaveColorViewController: UIViewController, UITextFieldDelegate {
     
     var colorToSave : UIColor?
     
+   
+    @IBOutlet weak var colorNameText: UITextField!
+    @IBAction func saveColor(_ sender: Any) {
+        self.colorNameText.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
